d9678f5d56eaa3ee4549123fabdfe690805d6508
Tej Baidwan <w0806279@myscc.ca>
2025-11-24 16:21:05 -0500

    Fix

--- dist/config/prismaClient.d.ts
+++ dist/config/prismaClient.d.ts
@@ -1,3 +1,3 @@
-declare const prisma: import("../generated/prisma/internal/class.js").PrismaClient<never, import("../generated/prisma/internal/prismaNamespace.js").GlobalOmitConfig | undefined, import("@prisma/client/runtime/client.js").DefaultArgs>;
+declare const prisma: import("../generated/prisma/internal/class.ts").PrismaClient<never, import("../generated/prisma/internal/prismaNamespace.ts").GlobalOmitConfig | undefined, import("@prisma/client/runtime/client").DefaultArgs>;
 export default prisma;
 //# sourceMappingURL=prismaClient.d.ts.map
