6be6ab7e4bcb42f203e7a938f3541e94e3e44006
Tej Baidwan <w0806279@myscc.ca>
2025-11-24 20:41:36 -0500

    feat(auth): Create the /register endpoint Created the /register endpoint by implementing the logic in the register method found in the AuthController, and then adding the corresponding /register route Closes #1

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,3 +3,71 @@ import bcrypt from 'bcrypt'
 import jwt from "jsonwebtoken";
 import { validationResult } from 'express-validator'
 import express from "express";
+
+// Register logic to create a new User
+export const register = async (req: express.Request, res: express.Response) => {
+
+    try {
+
+        // Check if their are any validation errors before continuing
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        // Deconstruct the request payload and extract the credentials
+        const { email, password, password_confirmation } = req.body;
+
+        // Ensure that the required values have been provided
+        if (!email || !password || !password_confirmation) {
+            return res.status(400).json({
+                message: "Missing email or password",
+            })
+        }
+
+        // Check if the password and password_confirmation match
+        if(password !== password_confirmation) {
+            return res.status(400).json({
+                message: "Passwords don't match",
+            })
+        }
+
+        // Attempt to find an existing user with this email
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        })
+
+        // Indicate if an account with this email exists already
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists",
+            })
+        }
+
+        // Create the salt to hash the password and hash it
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        // Create a User object with the given email and hashed password
+        const newUser = await prisma.user.create({
+            data: {
+                email,
+                passwordHash,
+            }
+        })
+
+        // Return the success response
+        return res.status(201).json({
+            message: "User created successfully",
+            user: newUser.id
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Server error",
+        })
+    }
+}
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,31 @@
 import express from 'express';
 import { body } from 'express-validator'
+import { register } from "../controllers/AuthController.ts";
 
 // Express router paths for auth routes
 const router = express.Router();
+
+router.post("/register",
+    [
+    body("email").isEmail().trim().toLowerCase().
+        withMessage("Please enter a valid email").
+        matches(/@uwindsor\.ca$/).
+        withMessage("Please enter a valid @uwindsor email"),
+    body("password").isLength({ min: 8 }).
+    withMessage("Password must be at least 8 characters").
+         matches(/[a-z]/)
+        .withMessage('Password must contain a lowercase letter')
+        .matches(/[A-Z]/)
+        .withMessage('Password must contain an uppercase letter')
+        .matches(/\d/)
+        .withMessage('Password must contain a number.')
+        .matches(/[\W_]/)
+        .withMessage('Password must contain a special character'),
+    body("password_confirmation").isLength({ min: 8 }).
+    withMessage("Password must be at least 8 characters")
+],
+    register);
+
+
 
 export default router;
